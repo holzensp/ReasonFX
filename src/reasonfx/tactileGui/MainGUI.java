cb1bbdbde212d0dbea8630ae753dab1c5f95f81b
Shaedys <shaedys@gmail.com>
2015-01-21 15:17:28 +0100

    A simple start

--- src/reasonfx/tactileGui/MainGUI.java
+++ src/reasonfx/tactileGui/MainGUI.java
@@ -8,11 +8,21 @@ package reasonfx.tactileGui;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import nl.utwente.cs.caes.tactile.control.TactilePane;
 import nl.utwente.cs.caes.tactile.debug.DebugParent;
@@ -26,64 +36,52 @@ import reasonfx.term.Term;
  * @author holzensp
  */
 public class MainGUI extends Application {
-
-    private static final Rule andEL = LogicParser.parse(LogicParser::dedrule, "@x^@y |- @x").r,
-            andI = LogicParser.parse(LogicParser::dedrule, "@x, @y |- @x^@y").r;
-    private static final Term pAndQ = LogicParser.parse(LogicParser::proposition, "p ^ q").t,
-            termP = LogicParser.parse(LogicParser::proposition, "p").t;
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        TactilePane root;
-        try {
-//            root = (TactilePane) FXMLLoader.load(getClass().getResource("Main.fxml"));
-        root = (TactilePane) FXMLLoader.load(getClass().getResource("Main_1.fxml"));
-        
-            TextItem cItem = new TextItem();
-//            cItem.setColor(new Paint());
-//            TactilePane.setOnInProximity(circle, event -> {
-//                if (!TactilePane.isInUse(circle)) {
-//                    TactilePane.moveAwayFrom(circle, event.getOther(), 20);
-//                }
-//            });
-          
-            
-            Scene scene = new Scene(root);
-//            DebugParent debug = new DebugParent(root);
-//            debug.registerTactilePane(root);
-//            scene = new Scene(debug);
-            
-            stage.setFullScreen(false);
-            stage.setOnCloseRequest(event -> {
-                Platform.exit();
-            });
-            stage.setTitle("ReasonFX");
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(MainGUI.class.getName()).log(Level.SEVERE, null, ex);
-        }
-//        Group root = new Group();
-//
-        WantedView goal   = new WantedView(new Wanted(termP));
-//        RuleView   rAndEL = new RuleView(RuleInstance.instantiate(andEL));
-//        RuleView   rAndI  = new RuleView(RuleInstance.instantiate(andI));
-//        GivenView  premis = new GivenView(new Given(pAndQ));
-//        
-//        // Yuck!
-//        goal.relocate(  500, 600);
-//        rAndEL.relocate(500, 400);
-//        rAndI.relocate( 600, 400);
-//        premis.relocate(500, 300);
-//
-//        root.getChildren().addAll(goal, rAndEL.getWrapGroup(), rAndI.getWrapGroup(), premis.getWrapGroup());
-//        
-//        stage.setTitle("NatDuctGUI");
-//        stage.setScene(new Scene(root, 1200, 800));
-//        stage.show();
-    }
+	
+    static final int WIDTH = 800;
+    static final int HEIGHT = 600;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
+		
+		// Init TactilePane TODO: FXML
+		TactilePane tactilePane = new TactilePane();
+        tactilePane.setPrefSize(WIDTH, HEIGHT);
+        tactilePane.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        tactilePane.setBordersCollide(true);
+		
+		TextItem textPorQ = new TextItem("p v q");
+		textPorQ.setLayoutX(50);
+		textPorQ.setLayoutY(250);
+		tactilePane.getChildren().add(textPorQ);
+		
+		TextItem textP = new TextItem("p");
+		textP.setLayoutX(70);
+		textP.setLayoutY(100);
+		tactilePane.getChildren().add(textP);
+		
+        TextItem textQ = new TextItem("q");
+        textQ.setLayoutX(150);
+        textQ.setLayoutY(50);
+        tactilePane.getChildren().add(textQ);
+		
+		root.setCenter(tactilePane);
+		
+		Scene scene = new Scene(root);
+        primaryStage.setFullScreen(true);
+        primaryStage.setOnCloseRequest(event -> { Platform.exit(); });
+        primaryStage.setScene(scene);
+        primaryStage.show();
+		
+	}
+	
+	
+    
+ 
 
     public static void main(String args[]) {
         Application.launch(args);
     }
+
+
 }
