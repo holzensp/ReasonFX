87585b61a2cc7349cc4f2041b75238ed83bd90ea
Fedor Beets <fedorbeets@gmail.com>
2015-01-29 13:08:04 +0100

    Basic working example (has some bugs)

--- src/reasonfx/tactileGui/MainGUI.java
+++ src/reasonfx/tactileGui/MainGUI.java
@@ -26,8 +26,13 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import nl.utwente.cs.caes.tactile.control.TactilePane;
 import nl.utwente.cs.caes.tactile.debug.DebugParent;
+import reasonfx.gui.GivenView;
+import reasonfx.gui.RuleView;
+import reasonfx.gui.WantedView;
 import reasonfx.parsers.LogicParser;
+import reasonfx.rule.Given;
 import reasonfx.rule.Rule;
+import reasonfx.rule.RuleInstance;
 import reasonfx.rule.Wanted;
 import reasonfx.term.Term;
 
@@ -39,6 +44,13 @@ public class MainGUI extends Application {
 	
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
+    
+    private static final Rule
+    andEL = LogicParser.parse(LogicParser::dedrule,"@x^@y |- @x").r,
+    andI  = LogicParser.parse(LogicParser::dedrule,"@x, @y |- @x^@y").r;
+    private static final Term
+    pAndQ = LogicParser.parse(LogicParser::proposition,"p ^ q").t,
+    termP = LogicParser.parse(LogicParser::proposition,"p").t;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -50,27 +62,60 @@ public class MainGUI extends Application {
         tactilePane.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
         tactilePane.setBordersCollide(true);
 		
-		TextItem textPorQ = new TextItem("p v q");
-		textPorQ.setLayoutX(50);
-		textPorQ.setLayoutY(250);
-		tactilePane.getChildren().add(textPorQ);
+//		TextItem textPorQ = new TextItem("p v q");
+//		textPorQ.setLayoutX(50);
+//		textPorQ.setLayoutY(250);
+//		tactilePane.getChildren().add(textPorQ);
+//		
+//		TextItem textP = new TextItem("p");
+//		textP.setLayoutX(70);
+//		textP.setLayoutY(100);
+//		tactilePane.getChildren().add(textP);
+//		
+//        TextItem textQ = new TextItem("q");
+//        textQ.setLayoutX(150);
+//        textQ.setLayoutY(50);
+//        tactilePane.getChildren().add(textQ);
+        
+        //copy pasted from gui.MainGUI
+        WantedView goal   = new WantedView(new Wanted(termP));
+        RuleView   rAndEL = new RuleView(RuleInstance.instantiate(andEL));
+        RuleView   rAndI  = new RuleView(RuleInstance.instantiate(andI));
+        GivenView  premis = new GivenView(new Given(pAndQ));
+        
+        // Yuck!
+        goal.relocate(  500, 600);
+        rAndEL.relocate(500, 400);
+        rAndI.relocate( 600, 400);
+        premis.relocate(500, 300);
+
+        tactilePane.getChildren().addAll(goal, rAndEL.getWrapGroup(), rAndI.getWrapGroup(), premis.getWrapGroup());
 		
-		TextItem textP = new TextItem("p");
-		textP.setLayoutX(70);
-		textP.setLayoutY(100);
-		tactilePane.getChildren().add(textP);
-		
-        TextItem textQ = new TextItem("q");
-        textQ.setLayoutX(150);
-        textQ.setLayoutY(50);
-        tactilePane.getChildren().add(textQ);
-		
+        
+        // Key bindings
+        primaryStage.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+        	@Override
+        	public void handle(KeyEvent keyEvent){
+                if (keyEvent.getCode() == KeyCode.F11) {
+                	if (primaryStage.isFullScreen()){
+                		primaryStage.setFullScreen(false);
+                	} else {
+                		primaryStage.setFullScreen(true);
+                	}
+                	keyEvent.consume();
+                }
+            }
+        	
+        });
+        
+        
 		root.setCenter(tactilePane);
 		
 		Scene scene = new Scene(root);
         primaryStage.setFullScreen(true);
         primaryStage.setOnCloseRequest(event -> { Platform.exit(); });
         primaryStage.setScene(scene);
+        primaryStage.setTitle("NatDuctGUI");
         primaryStage.show();
 		
 	}
